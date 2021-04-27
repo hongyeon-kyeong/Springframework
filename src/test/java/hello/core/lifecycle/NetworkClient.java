e1df7634265d56ce98b9164cce4f6d00a7030a81
hongyeonkyeong <hyjykelly@gmail.com>
2021-04-27 20:22:13 +0900

    20210427

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,11 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -15,7 +21,7 @@ public class NetworkClient {
 
     //서비스 시작시 호출
     public void connect() {
-        System.out.println(" connect :  " + url);
+        System.out.println("connect :  " + url);
     }
 
     public void call(String message) {
@@ -25,5 +31,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
     }
 }
